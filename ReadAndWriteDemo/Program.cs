6782d0a13d478fb9a8c295ac16a3e10b17b7b7e9
Peter Coker <petercoker@protonmail.com>
2019-01-16 18:02:58 +0000

    complete file stream exercise

---
+++ ReadAndWriteDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace ReadAndWriteDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteForFile();
+            ReadForFile();
+        }
+
+        public static void WriteForFile()
+        {
+            StreamWriter writer = new StreamWriter(@"C: \Users\sonol\Downloads\ReadAndWriteDemo\ReadWrite\Demo.txt");
+            writer.WriteLine("Hello world");
+            writer.Close();
+        }
+
+        public static void ReadForFile()
+        {
+            StreamReader reader = new StreamReader(@"C: \Users\sonol\Downloads\ReadAndWriteDemo\ReadWrite\Demo.txt");
+            Console.WriteLine(reader.ReadToEnd());
+            reader.Close();
+        }
+    }
+}
